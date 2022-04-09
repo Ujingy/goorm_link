9faa9c49408790d3aa58332e68f8d3c360424712
Ujingy <hyjing72@naver.com>
2022-04-09 05:47:06 +0000

    adding components

--- src/components/Todolist_copy0.vue
+++ src/components/Todolist_copy0.vue
@@ -7,16 +7,16 @@
             <!-- ul.list_add -->
             <ul class="list_add">
                 <li class="list_add_tit"><p>등록 하기</p></li>
-                <li><p><input type="text" v-model="todo" @keyup.enter = 'addTodo' placeholder="여기에 할일을 적으세요"><button @click="listUp">등 록</button></p></li>
+                <li><p><input type="text"><button>등 록</button></p></li>
             </ul>
             <!--// ul.list_add -->
 
             <!-- ul.todo_list -->
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
-                <li class="del_btn" v-for="(item, index) in todo" :key="index">
+                <li class="del_btn" v-for="(item, index) in todolist" :key="index">
                     <p>
-                        <input type="checkbox" name="check1" value=""> {{ item }} 
+                        <input type="checkbox" name="check1" value=""> {{ item.todo }} 
                     </p>
 										<ul class="todo_list_btn">
                         <li><button @click="moveClick">이 동</button></li>
@@ -37,30 +37,20 @@
 </template>
 
 <script>
-import { reactive, toRefs, inject, ref } from 'vue'
-	
 export default {
 	name: 'todolist',
 	data: function() {
 		return {
-			todo: '',
-			todos: [],
+			todolist: [
+				{todo: "Vue.js 정리하기"},
+				{todo: "Vue.js 공부하기"},
+				{todo: "수업 준비하기"},
+				{todo: "책 읽기"},
+				{todo: "보고서 작성하기"},
+			]
 		}
 	},
 	methods: {
-		addTodo() {
-			if (this.todo) {
-				var todolist = {
-					completed: false,
-					item: this.todo
-				}
-				localStorage.setItem(this.todo, JSON.stringify(todolist))
-				this.clearInput()
-			}
-		},
-		clearInput() {
-			this.todo = '';
-		},
 		moveClick() {
 			console.log("move");
 		},
